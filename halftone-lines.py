3ff841af2d8c40ebba9651e798fd9a68aed3ef9d
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2021-11-25 22:50:07 +0000

    Sigmoid kinda working (slope is not changing the output image)

--- halftone-lines.py
+++ halftone-lines.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw
-import cv2
-from bezier import Polygon
+from sigmoid import SigmoidPolygon
 import numpy as np
+import cv2
 
 def get_intensity(square):
     return 1-square.mean()/255
@@ -23,59 +23,11 @@ if __name__ == "__main__":
     print(n_row, n_col, n_row*n_col)
     
     for y in range(n_col):
-        line      = Polygon(y*side)
-        intensity = get_intensity( img[y*kernel_s:(y+1)*kernel_s, 0:kernel_s] )
-        height    = intensity*side
-        print(f"y[{y*kernel_s}:{(y+1)*kernel_s}], x[{0}:{kernel_s}], {height}")
-        y_top     = (side-height)//2
-        y_bot     = height + y_top
-        line.extend([(0,y_bot), (0,y_top)])
-        line.extend_bottom((0,y_bot))
-        line.extend_bottom((side//2,y_bot))
-        line.extend_top((0,y_top))
-        line.extend_top((side//2,y_top))
+        line = SigmoidPolygon(y*side, side, slope = 0.0000001, N = 20)
         for x in range(1,n_row-1):
-            intensity   = get_intensity( img[y*kernel_s:(y+1)*kernel_s, x*kernel_s:(x+1)*kernel_s] )
-            print(f"y[{y*kernel_s}:{(y+1)*kernel_s}], x[{x*kernel_s}:{(x+1)*kernel_s}], {height}")
-            # print(x,y, intensity)
-            height      = intensity*side
-            y_top     = (side-height)//2
-            y_bot     = height + y_top
-            line.extend_bottom((side//2 + x*side,y_bot))
-            line.extend_top((side//2 + x*side,y_top))
-        x = n_row-1
-        intensity   = get_intensity( img[y*kernel_s:(y+1)*kernel_s, x*kernel_s:(x+1)*kernel_s] )
-        print(f"y[{y*kernel_s}:{(y+1)*kernel_s}], x[{x*kernel_s}:{(x+1)*kernel_s}], {height}")
-        height      = intensity*side
-        y_top     = (side-height)//2
-        y_bot     = height + y_top
-        line.extend_bottom((side//2 + x*side,y_bot))
-        line.extend_bottom((n_col*side-1,y_bot))
-        line.extend_top((side//2 + x*side,y_top))
-        line.extend_top((n_col*side-1,y_top))
-        line.extend( line.top )
-        line.extend([(n_col*side-1,y_top), (n_col*side-1,y_bot)])
-        line.extend( line.bottom[::-1] )
-        line.draw( draw )
+            intensity = get_intensity( img[y*kernel_s:(y+1)*kernel_s, x*kernel_s:(x+1)*kernel_s] )
+            height    = intensity*side
+            line.height(x*side, height)
+        line.draw(draw)
+        # break
     img_out.save("out.png")
-    # im      = Image.new("RGB", (200, 100), (255, 255, 255)) 
-    # draw    = ImageDraw.Draw(im)
-    # p       = Polygon(100)
-    # 
-    # p.extend([(0,80), (0,20)])
-    # p.extend([(0,20), (50,25), (150,40), (199,35)])
-    # p.extend([(199,35), (199,65)])
-    # p.extend([(0,80), (50,75), (150,60), (199,65)][::-1])
-    # 
-    # p.draw(draw)
-    # 
-    # # draw.ellipse((0,20,1,21), fill = "black")
-    # # draw.ellipse((50,25,51,26), fill = "black")
-    # # draw.ellipse((150,40,151,41), fill = "black")
-    # # draw.ellipse((199,35,200,36), fill = "black")
-    # # 
-    # # draw.ellipse((0,80,1,81), fill = "black")
-    # # draw.ellipse((50,75,51,76), fill = "black")
-    # # draw.ellipse((150,60,151,61), fill = "black")
-    # # draw.ellipse((199,65,200,66), fill = "black")
-    # im.save("out.png")
--- sigmoid.py
+++ sigmoid.py
@@ -1,7 +1,8 @@
+import numpy as np
+
 class SigmoidPolygon:
     def __init__(self, y: int, max_height: int, slope: float = 1., N: int = 10):
-        N                   = N//2
-        self.ts             = [t/N for t in range(-N, N)]
+        self.N              = N
         self.y              = y
         self.max_height     = max_height
         self.slope          = slope
@@ -9,14 +10,18 @@ class SigmoidPolygon:
         self.bottom_line    = []
         self.points         = []
 
-    def make_sigmoid(self, pair):
-        pass
+    def make_sigmoid(self, start, end):
+        x_start, y_start    = start
+        x_end, y_end        = end
+        xs                  = np.arange(x_start, x_end, (x_end-x_start)/self.N)
+        sigmoid             = lambda x: (y_end-y_start)*(1/(1 + np.exp(-x*self.slope))) + y_start
+        return [(x,sigmoid(x)) for x in xs]
 
     def height(self, x, height):
         y_top  = (self.max_height-height)/2
         y_bot  = height + y_top
-        self.top_line.append((x,y_top))
-        self.bottom_line.append((x,y_bot))
+        self.top_line.append((x,y_top+self.y))
+        self.bottom_line.append((x,y_bot+self.y))
 
     def compute_points(self):
         first_y_top     = self.top_line[0][1]
@@ -26,7 +31,7 @@ class SigmoidPolygon:
         self.points     = [(0,first_y_bottom), (0,first_y_top)]
         for i in range(len(self.top_line)-1):
             self.points.extend( self.make_sigmoid(self.top_line[i],self.top_line[i+1]) )
-        last_x = len(self.top_line)*max_height - 1
+        last_x = len(self.top_line)*self.max_height - 1
         self.points.extend([(last_x,last_y_top), (last_x,last_y_bottom)])
         for i in reversed(range(len(self.bottom_line)-1)):
             self.points.extend( self.make_sigmoid(self.bottom_line[i],self.bottom_line[i+1])[::-1] )
@@ -34,12 +39,12 @@ class SigmoidPolygon:
     def draw(self, draw, color: str = "black"):
         if len(self.points) == 0:
             self.compute_points()
+        # print(self.points)
         draw.polygon(self.points, fill = color)
 
 
-if __main__ == "__main__":
+if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    import numpy as np
 
     Y_TOP = 0
     Y_BOT = 1
